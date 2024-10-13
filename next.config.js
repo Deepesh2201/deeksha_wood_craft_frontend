9844d14c31f20d96b657e90d8c977ba0478a0652
Deepesh Raj <deepeshinfo22@gmail.com>
2024-10-14 00:19:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "laravel.pixelstrap.net/fastkart",
+        hostname: "laravel.pixelstrap.net",
       },
       { 
         protocol: "http",
