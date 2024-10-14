f8ecd161a7af77a7afb6c197f1f4f14e06603d63
Deepesh Raj <deepeshinfo22@gmail.com>
2024-10-14 14:40:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: { 
-    baseUrl: "http://localhost:3001/", 
+    baseUrl: "https://dwc-liart.vercel.app/", 
     URL: "https://dwcapi.shinewell.in/public/api",
     storageURL:"https://dwcapi.shinewell.in",
   },
