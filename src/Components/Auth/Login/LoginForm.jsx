d17d8bd94de5fa7be8c9e50503b5931db802bba7
Deepesh Raj <deepeshinfo22@gmail.com>
2024-10-14 14:26:42 +0530

    Update LoginForm.jsx

--- src/Components/Auth/Login/LoginForm.jsx
+++ src/Components/Auth/Login/LoginForm.jsx
@@ -18,11 +18,11 @@ const LoginForm = ({mutate, isLoading}) => {
     <>
       
       <Formik
-        // initialValues={{
-        //   email: '',
-        //   password: '',
-        //   recaptcha: ''
-        // }}
+        initialValues={{
+          email: '',
+          password: '',
+          recaptcha: ''
+        }}
         validationSchema={YupObject({
           email: emailSchema,
           password: loginPasswordSchema,
